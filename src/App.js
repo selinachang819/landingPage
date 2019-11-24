f5ea5f4bdf175f9a6344f868eea2057be13e20ed
unknown <ycc525@nyu.edu>
2019-11-25 02:08:38 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -6,66 +6,22 @@ import './App.css';
 function App() {
   return (
   <div className="App">
-    <img className="mainImage" src="https://bit.ly/2O1vmW9"/>
-    <img class="swipeUP" src="https://bit.ly/2NFNO7E"/>
-      <div className="text">
-      <div className="typewriter-text"> Have you even been "walking-distracted" by phone?</div>
-      <div id="bullean">
-        <p>
-        <input id="Yes" type="checkbox" onClick={followUP} ></input>
-        <label>Definitely YES.</label></p>
-        <p>
-          <input id="No" type="checkbox" onClick={opposite} ></input>
-          <label>Of course NO!</label>
-        </p>
+    <a href="mailto:ycc525@nyu.edu">Contact Us!</a>
+    <img src="https://bit.ly/2XIAS4d"/>
+    <p id="title">Your Stair Safety Guard</p>
+    <div className="description">  
+      <div className="typewriter">
+        <h3 className="typewriter-text">Keep your phone DOWN and head UP</h3>
       </div>
-      <div id="followUP" >
-      <div className="typewriter2">
-        <p className="typewriter-text2">20% of people have endangered themselves on stairs by sticking to their cellphones.</p>
-      </div>
-        <div className="form">   
-        <div>Save yourself by signing up:</div> 
-        <input id="name" type="text" placeholder="Your Name"></input>
-        <input id="concern" type="text" placeholder="Your Concern"></input>
-        <button >Submit!</button>
-        </div>
-        
-      </div>
-      <div id="opposite">Congratulations!!!</div>
-      </div>
-  </div>
-  
-  );
-  }
- 
-
-
-
-function followUP(){
-  
-  let checkBox = document.getElementById("Yes");
-  let text = document.getElementById("followUP");
-  // If the checkbox is checked, display the output text
-  if(checkBox.checked == true){
-    text.style.visibility="visible";
-  }else{
-    text.style.visibility="hidden";
-  }
+        <p>Have you ever been walking-distracted by phone?</p>
+        <p>Free your hand from swiping your phone while on stairs!</p>
+        <button onClick={jumpPage}>Download</button>
+    </div>
+    </div>
+  )
+  };
+function jumpPage(){
+  alert("This will automatically download our app, continue?")
 }
-
-function opposite(){
-  
-  let checkBox2 = document.getElementById("No");
-  let text2 = document.getElementById("opposite");
-  // If the checkbox is checked, display the output text
-  if(checkBox2.checked == true){
-    text2.style.visibility="visible";
-  }else{
-    text2.style.visibility="hidden";
-  }
-}
-
-
- 
 
 export default App;
